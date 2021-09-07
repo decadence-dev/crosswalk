02de41c05f459ff7bb1467fb6f9215dd128d2ad0
Anton Petrov <marblemonolit@gmail.com>
2021-09-07 23:11:49 +0300

    Map component created.

--- client/src/components/map.vue
+++ client/src/components/map.vue
@@ -1,11 +1,39 @@
 <script>
+import MenuIcon from './icons/menu'
+import NotificationsIcon from './icons/notifications'
+
 export default {
-  name: 'Map'
+  name: 'Map',
+  components: {MenuIcon, NotificationsIcon}
 }
 </script>
 
 <template lang="html">
   <div class="map">
-    <h1>Some sort of text</h1>
+    <div class="header">
+      <MenuIcon></MenuIcon>
+      <NotificationsIcon></NotificationsIcon>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+  @import "src/styles/variables";
+  .map {
+    position: relative;
+    display: flex;
+    flex-flow: column wrap;
+    flex: 1 0 auto;
+    max-width: 100%;
+    background-color: $black;
+  }
+
+  .header {
+    position: absolute;
+    display: flex;
+    justify-content: space-between;
+    top: 16px;
+    left: 16px;
+    right: 16px;
+  }
+</style>
