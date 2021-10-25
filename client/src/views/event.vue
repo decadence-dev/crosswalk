b22c4655c44e3ca2070c4bc30ff490cf85bcd69b
Anton Petrov <marblemonolit@gmail.com>
2021-10-25 20:34:22 +0300

    createdDate transformation fixed.

--- client/src/views/event.vue
+++ client/src/views/event.vue
@@ -25,7 +25,10 @@ export default {
       const { createdDate } = this.$store.state.event;
       if (createdDate !== undefined) {
         const dt = new Date(createdDate);
-        return `${dt.getFullYear()}.${dt.getUTCMonth().toString().padStart(2, '0')}.${dt.getDate().toString().padStart(2, '0')}`;
+        const year = dt.getFullYear();
+        const month = (dt.getUTCMonth() + 1).toString().padStart(2, '0');
+        const day = dt.getDate().toString().padStart(2, '0');
+        return `${year}.${month}.${day}`;
       }
       return '';
     },
